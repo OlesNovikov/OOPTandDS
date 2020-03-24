a739bc0912d81ccd35897b5b98b890d61dddf5df
OlesNovikov <55893703+OlesNovikov@users.noreply.github.com>
2020-03-24 20:37:26 +0300

    Add files via upload

---
+++ DrawFigures/DrawFigures/Line.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace DrawFigures
+{
+    class Line : Shape
+    {
+        public Point pt1;
+        public Point pt2;
+        public Pen pen;
+
+        public Line (Pen pen, Point pt1, Point pt2) 
+        {
+            this.pen = pen;
+            this.pt1 = pt1;
+            this.pt2 = pt2;
+        }
+
+        public override void Draw(Graphics graphics)
+        {
+            graphics.DrawLine(pen, pt1, pt2);
+        }
+    }
+}
